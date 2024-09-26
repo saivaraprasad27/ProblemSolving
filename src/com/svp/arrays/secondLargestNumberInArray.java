56189d57af2a753d3b8df49a12898d31b17342c6
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2024-09-26 10:23:34 +0530

    bruteApproach

--- src/com/svp/arrays/secondLargestNumberInArray.java
+++ src/com/svp/arrays/secondLargestNumberInArray.java
@@ -1,5 +1,7 @@
 package com.svp.arrays;
 
+import java.util.Arrays;
+
 //optimized solution
 //Second Largest
 public class secondLargestNumberInArray {
@@ -21,5 +23,45 @@ public class secondLargestNumberInArray {
         int [] arr ={-1,-2,-3,0,1,2,3};
         int n = arr.length;
         System.out.println(secondLargestNumberInArray(arr,n));
+    }
+}
+
+//optimized
+//second smallest number
+class secondSmallestNumber{
+    static int secondSmallestNum(int[] arr,int n){
+        int small = Integer.MAX_VALUE;
+        int secondSmall = Integer.MAX_VALUE;
+        for (int i=0;i < n; i++){
+            if (arr[i] < small){
+                secondSmall = small;
+                small = arr[i];
+            }
+            else if (arr[i] < secondSmall && arr[i]!=small){
+                secondSmall = arr[i];
+            }
+        }
+        return secondSmall;
+    }
+    public static void main(String[] args){
+        int [] arr ={-1,-2,-3,0,1,2,3};
+        int n = arr.length;
+        System.out.println(secondSmallestNum(arr,n));
+    }
+}
+
+//Extreme brute
+//small and large element
+class secondLarNumber{
+    static int [] bruteSecondLargestNumber(int [] arr, int n){
+        Arrays.sort(arr);
+        int large = arr[n-2];
+        int small = arr[1];
+        return new int[] {small,large};
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6};
+        int n = arr.length;
+        System.out.println(Arrays.toString(bruteSecondLargestNumber(arr,n)));
     }
 }
