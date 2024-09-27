e4fb12cf5795df735f217ac3c2ccab23bfbbddd2
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2024-09-27 23:12:39 +0530

    brutesolutionPlusone

--- src/com/svp/arrays/pulsOneProblem.java
+++ src/com/svp/arrays/pulsOneProblem.java
@@ -2,6 +2,7 @@ package com.svp.arrays;
 
 import java.util.Arrays;
 
+//optimalSolution
 public class pulsOneProblem {
     static int[] plusOneSol(int[] arr){
         int n = arr.length;
@@ -20,5 +21,41 @@ public class pulsOneProblem {
         int [] arr = {9};
         //int arr [] = {1,2,3,4};
         System.out.println(Arrays.toString(plusOneSol(arr)));
+    }
+}
+
+class pulsOne{
+    static int[] plusOneBruteSolution(int [] arr){
+        reverseArray(arr);
+        int n = arr.length;
+        for(int i =0;i < n; i++){
+            arr[i]+=1;
+            if (arr[i] < 10){
+                reverseArray(arr);
+                return arr;
+            }
+            else {
+                arr[i] = 0;
+            }
+        }
+        int [] newArr = new int [arr.length+1];
+        newArr[0] = 1;
+        return newArr;
+    }
+    //reverseArray
+    public static void reverseArray(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args){
+        int arr [] = {1,2,3,4};
+        System.out.println(Arrays.toString(plusOneBruteSolution(arr)));
     }
 }
