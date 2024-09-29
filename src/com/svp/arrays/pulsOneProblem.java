95b027ab4db1114a7a7f0621e71633760c87bd4e
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2024-09-29 19:44:37 +0530

    line added

--- src/com/svp/arrays/pulsOneProblem.java
+++ src/com/svp/arrays/pulsOneProblem.java
@@ -57,5 +57,6 @@ class pulsOne{
     public static void main(String[] args){
         int arr [] = {1,2,3,4};
         System.out.println(Arrays.toString(plusOneBruteSolution(arr)));
+        System.out.println("hello");
     }
 }
