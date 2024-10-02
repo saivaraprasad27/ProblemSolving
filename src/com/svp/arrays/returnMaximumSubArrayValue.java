0b4dd7aea295a7808cfb738d4b9dca7fd00baad5
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2024-10-02 22:50:02 +0530

    kadanesAlgorithm maximum subarray

---
+++ src/com/svp/arrays/returnMaximumSubArrayValue.java
@@ -0,0 +1,21 @@
+package com.svp.arrays;
+
+public class returnMaximumSubArrayValue {
+    public static int kadanesAlgorithm(int []arr,int n){
+        if (n == 1) return arr[0];
+        int count = 0;
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+        for(int i = 0;i < n; i++){
+            sum += arr[i];
+            if (sum > maxi) maxi = sum;
+            if (sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+    public static void main(String[] args){
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        System.out.println(kadanesAlgorithm(arr,n));
+    }
+}
