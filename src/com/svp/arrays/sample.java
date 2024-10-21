ea1aab60d87af20616cdec86380715f4e40a5ba3
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2024-10-21 19:52:28 +0530

    sample check

--- src/com/svp/arrays/sample.java
+++ src/com/svp/arrays/sample.java
@@ -3,5 +3,7 @@ package com.svp.arrays;
 public class sample {
     public static void main(String[] args){
         System.out.println("sample check");
+        System.out.println("check once branch change");
+        System.out.println("check once branch change");
     }
 }
